d8171a292a8e4980dff9b9943d6e99a952697ecc
FleaKskrrt <73479466+FleaKskrrt@users.noreply.github.com>
2021-06-01 12:31:40 +0200

    freshindex

--- index.php
+++ index.php
@@ -8,10 +8,10 @@ require("header.php");
 
 <img src="pic2.png" alt="pic1" class="img-fluid max-width: 100% height: auto">
 
-<main role="main" class="inner cover">
-        <h1 class="cover-heading">Sell or buy now</h1>
-        <p class="lead">Cover is a one-page template for building simple and beautiful home pages. <br> Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+<main role="main" class="d-flex justify-content-center inner cover">
+        <h1 class="mt-5 cover-heading">Sell or buy now</h1>
+        <p class="lead">With IKEA Auctions you can sell and buy used IKEA products. <br> Register now and help the climate through second hand IKEA funiture - <br> list items and buy other great products.</p>
         <p class="lead">
-          <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
+          <a href="#" class="mt-5 btn btn-lg btn-secondary">Learn more</a>
         </p>
       </main>
